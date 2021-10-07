4c410115238746090fba578ee6af2c5a9a3a80e0
jempitman <80392341+jempitman@users.noreply.github.com>
2021-10-07 16:28:12 +0200

    Refactor: Increment by Identity

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/entity/Pet.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/entity/Pet.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 public class Pet {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private PetType type;
