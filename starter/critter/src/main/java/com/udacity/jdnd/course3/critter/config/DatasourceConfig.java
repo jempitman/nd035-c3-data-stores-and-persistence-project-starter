bdd9d428b7a16cf96d9ff4b26c8ff3c693804d72
jempitman <80392341+jempitman@users.noreply.github.com>
2021-10-11 13:26:32 +0200

    Refactor: Suppress DatasourceConfig

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/config/DatasourceConfig.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/config/DatasourceConfig.java
@@ -1,13 +1,6 @@
 package com.udacity.jdnd.course3.critter.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-import javax.sql.DataSource;
-
+/*
 @Configuration
 public class DatasourceConfig {
 
@@ -27,3 +20,5 @@ public class DatasourceConfig {
     }
 
 }
+
+ */
