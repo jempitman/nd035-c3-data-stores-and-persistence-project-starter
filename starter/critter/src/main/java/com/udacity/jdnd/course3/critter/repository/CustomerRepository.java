999f7cfac18f9aa8540e7aef9e145d68551b81ea
jempitman <80392341+jempitman@users.noreply.github.com>
2021-10-13 14:53:33 +0200

    Refactor: replace missing @Repository annotations

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/CustomerRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/CustomerRepository.java
@@ -2,10 +2,12 @@ package com.udacity.jdnd.course3.critter.repository;
 
 import com.udacity.jdnd.course3.critter.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Jpa Repository to store Customer entities
  */
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 }
--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/EmployeeRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/EmployeeRepository.java
@@ -2,9 +2,16 @@ package com.udacity.jdnd.course3.critter.repository;
 
 import com.udacity.jdnd.course3.critter.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.DayOfWeek;
+import java.util.List;
 
 /**
  * Jpa repository to store Employee entities
  */
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    public List<Employee> findByDaysAvailable(DayOfWeek requestedDay);
 }
