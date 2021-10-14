323c204ffff89f3186430175ee5b26c1cb4a0dd4
jempitman <80392341+jempitman@users.noreply.github.com>
2021-10-14 14:05:03 +0200

    Feat: find petList by customerId

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/PetRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/PetRepository.java
@@ -4,10 +4,14 @@ import com.udacity.jdnd.course3.critter.entity.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Jpa repository to hold Pet entities
  */
 
 @Repository
 public interface PetRepository extends JpaRepository<Pet, Long> {
+
+    List<Pet> findByCustomerId(long ownerId);
 }
