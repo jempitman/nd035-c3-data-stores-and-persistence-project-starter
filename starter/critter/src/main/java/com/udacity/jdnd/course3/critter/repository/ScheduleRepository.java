e012d258338291324109246b14ef600c374f0608
jempitman <80392341+jempitman@users.noreply.github.com>
2021-11-22 19:26:01 +0100

    Chore: clean up redundant code

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepository.java
@@ -20,44 +20,4 @@ public interface ScheduleRepository {
     List<Schedule> findScheduleByEmployee(Employee employee);
 
 
-//    private static final String FIND_ALL_SCHEDULES = "select s from Schedule s";
-//    private static final String FIND_SCHEDULE_BY_PET = "select s from Schedule s where :pet member of s.pets";
-//    private static final String FIND_SCHEDULE_BY_EMPLOYEE = "select s from Pet p where :customer member of p.customers";
-//
-//
-//
-//    public Schedule findSchedule(Long id){
-//        return entityManager.find(Schedule.class, id);
-//    }
-//
-//    public Schedule mergeSchedule(Schedule schedule){
-//        return entityManager.merge(schedule);
-//    }
-//
-//    public void deleteSchedule(Long id){
-//        Schedule schedule = entityManager.find(Schedule.class, id);
-//        entityManager.remove(schedule);
-//    }
-//
-//    public List<Schedule> getAllSchedules(){
-//        TypedQuery<Schedule> query = entityManager.createQuery(FIND_ALL_SCHEDULES, Schedule.class);
-//        return query.getResultList();
-//    }
-//
-//    public List<Schedule> findScheduleByPet(Pet pet){
-//        return entityManager
-//                .createQuery(FIND_SCHEDULE_BY_PET, Schedule.class)
-//                .setParameter("pet", pet)
-//                .getResultList();
-//    }
-//
-//    public List<Schedule> findScheduleByEmployee(Employee employee){
-//        return entityManager
-//                .createQuery(FIND_SCHEDULE_BY_EMPLOYEE, Schedule.class)
-//                .setParameter("employee", employee)
-//                .getResultList();
-//    }
-
-
-
 }
--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepositoryImpl.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepositoryImpl.java
@@ -17,8 +17,9 @@ public class ScheduleRepositoryImpl implements ScheduleRepository{
     EntityManager entityManager;
 
     private static final String FIND_ALL_SCHEDULES = "select s from Schedule s";
-    private static final String FIND_SCHEDULE_BY_PET = "select s from Schedule s where :pet member of s.pets";;
-    private static final String FIND_SCHEDULE_BY_EMPLOYEE = "select s from Schedule s where :employee member of s.employees";
+    private static final String FIND_SCHEDULE_BY_PET = "select s from Schedule s where :pet member of s.pets";
+    private static final String FIND_SCHEDULE_BY_EMPLOYEE = "select s from Schedule s where :employee member " +
+            "of s.employees";
 
 
     @Override
